57d55a8fb63e9de5f05563d83701c3b374872aed
minji10 <ch991012@naver.com>
2025-01-09 16:48:44 +0900

    Day_2

---
+++ C_class/변수의 출력.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int number1 = 1; // number1 이라는 것을 통해 1에 접근이 가능하다;
+	printf("%d",number1); // %d = printf 의 f(포멧), 10진수 정수를 받는 서식 지정자;
+	return 0;
+}
---
+++ C_class/영어다이아몬드.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\t\tA\n\tB\t\tC\nD\t\tE\t\tF\n\tG\t\tH\n\t\tI\n\n");
+	printf("\t\tA\n");
+	printf("\tB\t\tC\n");
+	printf("D\t\tE\t\tF\n");
+	printf("\tG\t\tH\n");
+	printf("\t\tI");
+	return 0;
+}
